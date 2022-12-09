044bfa860471ba58a0843d57c26cbb48a1b98a8f
joaovitor644 <jv644@protonmail.com>
2022-12-09 18:59:49 -0300

    Pequena mudança

--- src/main.jsx
+++ src/main.jsx
@@ -15,8 +15,6 @@ const router = createBrowserRouter([
   },
 ]);
 
-console.log(import.meta.env.VITE_API_KEY)
-var teste = import.meta.env.VITE_API_KEY
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Carrocel />
