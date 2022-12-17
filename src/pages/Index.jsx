3be26f903e72f7810a7e9eb2ec05de837a8478dc
joaovitor644 <jv644@protonmail.com>
2022-12-17 19:44:56 -0300

    novo formulario de busca e outras modificações

--- src/pages/Index.jsx
+++ src/pages/Index.jsx
@@ -1,7 +1,7 @@
 import Banner from '../components/Banner'
 import Carrocel from '../components/Carrocel';
 import Footer from '../components/Footer';
-
+import { Form } from 'react-router-dom';
 const key = import.meta.env.VITE_API_KEY 
 
 const urlCarrocelFilm = "https://api.themoviedb.org/3/movie/popular?api_key="+ key + "&language=pt-BR&page=1&region=BR"
@@ -11,14 +11,18 @@ export default function Index(){
     return (
         <>
             <Banner content={
-              <form id="search_form" action="/search" method="get" accept-charset="utf-8">
-                <h2>Bem-Vindo(a).</h2>
-                <h3>Milhões de Filmes, Séries e Pessoas para Descobrir. Explore já.</h3>
+              <Form action="/search" method="get">
+                <h2>Bem-Vindo(a) Ao FilmsAndSeries.</h2>
+                <h3>Milhões de Filmes e Séries  para Descobrir. Explore já.</h3>
                 <label className='search-bar'>
-                  <input dir="auto" id="search"  type="text" tabindex="1"  placeholder="Buscar por um Filme ou Série" />
-                  <input type="submit" id="subSearch"value="Search" />
+                <input dir="auto" id="search" name="keyword" type="text" tabindex="1"  placeholder="Buscar" />
+                <label htmlFor="">Pesquisar por: Filme</label>
+                <input type="radio" value="film" name='type' checked/>
+                <label htmlFor="">Série</label>
+                <input type="radio" value="serie" name='type'/>
+                <input type="submit" id="subSearch"value="Search" />
                 </label>
-              </form>
+              </Form>
             }/>
             <Carrocel title="Filmes Populares" url={urlCarrocelFilm} type="Film"/>
             <Carrocel title="Series Populares" url={urlCarrocelSerie} type="Series"/>
